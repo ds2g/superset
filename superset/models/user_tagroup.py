13ec2b28531864c305d34e247d23098cec12f520
Martin Limberger <martin.limberger@gmx.net>
2022-01-24 12:16:50 +0100

    fix user tagroup

--- superset/models/user_tagroup.py
+++ superset/models/user_tagroup.py
@@ -36,7 +36,4 @@ class UserTAGroup(Model, AuditMixinNullable):
     __tablename__ = "user_tagroup"
     id = Column(Integer, primary_key=True)
     user_id = Column(Integer, ForeignKey("ab_user.id"))
-    user = relationship(
-        security_manager.user_model, backref="extra_attributes", foreign_keys=[user_id]
-    )
     tagroup = Column(Integer)
